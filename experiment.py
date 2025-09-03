255e3d4c3cc17be677a09c8a5c96f5de80bfa50e
Edgar J. Andrade-Lotero <andradeed78@yahoo.com>
2025-09-03 13:21:22 -0500

    update experiment with simple positioning 1 forager

--- experiment.py
+++ experiment.py
@@ -5,14 +5,17 @@
 from markupsafe import Markup
 
 import psynet.experiment
-from psynet.modular_page import ImagePrompt, ModularPage, PushButtonControl, TextControl
-from psynet.timeline import Timeline, switch
+from psynet.modular_page import (
+    ImagePrompt, ModularPage, 
+    PushButtonControl, NumberControl,
+    NullControl
+)
+from psynet.timeline import Timeline
 from psynet.trial.create_and_rate import (
     CreateAndRateNode,
     CreateAndRateTrialMakerMixin,
     CreateTrialMixin,
     RateTrialMixin,
-    SelectTrialMixin,
 )
 from psynet.trial.imitation_chain import ImitationChainTrial, ImitationChainTrialMaker
 from psynet.utils import get_logger
@@ -20,28 +23,41 @@ from psynet.utils import get_logger
 logger = get_logger()
 
 
-def animal_prompt(text, img_url):
+##########################################################################################
+NUM_FORAGERS = 2
+##########################################################################################
+
+
+def positioning_prompt(text, img_url):
     return ImagePrompt(
         url=img_url,
         text=Markup(text),
-        width="300px",
+        width="475px",
         height="300px",
     )
 
 
-class CreateTrial(CreateTrialMixin, ImitationChainTrial):
+class CoordinatorTrial(CreateTrialMixin, ImitationChainTrial):
     time_estimate = 5
 
     def show_trial(self, experiment, participant):
+        forager_id = (participant.id - 1) % NUM_FORAGERS
+        # return PageMaker(
+        #         lambda: InfoPage(f"Where do you want to locate forager {forager_id}?"),
+        #         time_estimate=5,
+        #     )
         return ModularPage(
-            "create_trial",
-            animal_prompt(text="Describe the animal", img_url=self.context["img_url"]),
-            TextControl(),
-            time_estimate=self.time_estimate,
-        )
+                "create_trial",
+                positioning_prompt(
+                    text=f"Where do you want to locate forager {forager_id}?", 
+                    img_url=self.context["img_url"]
+                ),
+                NumberControl(),
+                time_estimate=self.time_estimate,
+            ),
 
 
-class SingleRateTrial(RateTrialMixin, ImitationChainTrial):
+class FarmerTrial(RateTrialMixin, ImitationChainTrial):
     time_estimate = 5
 
     def show_trial(self, experiment, participant):
@@ -52,33 +68,14 @@ class SingleRateTrial(RateTrialMixin, ImitationChainTrial):
         creation = self.get_target_answer(target)
         return ModularPage(
             "rate_trial",
-            animal_prompt(
-                text=f"How well does this description match the animal?<br><strong>{creation}</strong>",
+            positioning_prompt(
+                text=f"You have been located here:<br><strong>{target}</strong><br><strong>{creation}</strong>",
                 img_url=self.context["img_url"],
             ),
             PushButtonControl(
-                choices=[1, 2, 3, 4, 5],
-                labels=["not at all", "a little", "somewhat", "very", "perfectly"],
+                choices=[1],
+                labels=["Continue"],
                 arrange_vertically=False,
-            ),
-        )
-
-
-class SelectTrial(SelectTrialMixin, ImitationChainTrial):
-    time_estimate = 5
-
-    def show_trial(self, experiment, participant):
-        target_strs = [f"{target}" for target in self.targets]
-        answers = [self.get_target_answer(target) for target in self.targets]
-        return ModularPage(
-            "select_trial",
-            animal_prompt(
-                text="Which of these descriptions is the best?",
-                img_url=self.context["img_url"],
-            ),
-            PushButtonControl(
-                choices=target_strs,
-                labels=answers,
             ),
         )
 
@@ -92,37 +89,23 @@ class CreateAndRateTrialMaker(CreateAndRateTrialMakerMixin, ImitationChainTrialM
 ##########################################################################################
 
 
-def get_trial_maker(option):
-    rater_class = SingleRateTrial
-    n_creators = 2
+def get_trial_maker():
+    rater_class = FarmerTrial
+    n_creators = 1
     n_raters = 2
     rate_mode = "rate"
     include_previous_iteration = True
     target_selection_method = "one"
 
-    if option == "include_previous_iteration":
-        n_creators = 1
-        pass
-    elif option == "rate":
-        include_previous_iteration = False
-    elif option == "select":
-        rater_class = SelectTrial
-        n_raters = 3
-        target_selection_method = "all"
-        rate_mode = "select"
-    else:
-        raise ValueError(f"Unknown option: {option}")
-
-    seed_definition = "initial creation" if include_previous_iteration else {}
     start_nodes = [
-        CreateAndRateNode(context={"img_url": "static/dog.jpg"}, seed=seed_definition)
+        CreateAndRateNode(context={"img_url": "static/positioning.png"}, seed="initial creation")
     ]
 
     return CreateAndRateTrialMaker(
         n_creators=n_creators,
         n_raters=n_raters,
         node_class=CreateAndRateNode,
-        creator_class=CreateTrial,
+        creator_class=CoordinatorTrial,
         rater_class=rater_class,
         # mixin params
         include_previous_iteration=include_previous_iteration,
@@ -130,7 +113,7 @@ def get_trial_maker(option):
         target_selection_method=target_selection_method,
         verbose=True,  # for the demo
         # trial_maker params
-        id_=option + "_trial_maker",
+        id_="create_and_rate_basic",
         chain_type="across",
         expected_trials_per_participant=len(start_nodes),
         max_trials_per_participant=len(start_nodes),
@@ -147,32 +130,10 @@ def get_trial_maker(option):
     )
 
 
-available_demos = ["include_previous_iteration", "rate", "select"]
-branches = {demo_name: get_trial_maker(demo_name) for demo_name in available_demos}
-
-
 class Exp(psynet.experiment.Experiment):
     label = "Basic Create and Rate Experiment"
     initial_recruitment_size = 1
 
     timeline = Timeline(
-        ModularPage(
-            "pick_demo_page",
-            "Pick a demo you are interested in.",
-            PushButtonControl(
-                choices=available_demos,
-                labels=[
-                    "Rate creation + previous iteration",
-                    "Rate two creations",
-                    "Select from two creations + previous iteration",
-                ],
-            ),
-            time_estimate=1,
-        ),
-        switch(
-            "pick_demo_switch",
-            lambda participant: participant.answer,
-            branches=branches,
-            fix_time_credit=False,
-        ),
+        get_trial_maker(),
     )
